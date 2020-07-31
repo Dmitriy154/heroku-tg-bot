c656a53ec7bfe24457dad782b7d37d597719b7ad
Dmitriy154 <dmitriy154@gmail.com>
2020-07-31 21:11:10 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+//telegram_boot
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '1331879609:AAFiFjCz8BCARiy-xvR61tkozjQ0tKyFBQ0'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+	bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"`)
+})
