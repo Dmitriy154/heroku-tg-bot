b18adedcc47ca32320b71b9716a6e3b03a2bf3ae
Dmitriy154 <dmitriy154@gmail.com>
2020-08-01 08:59:33 +0300

    i_1

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const TOKEN = '1331879609:AAFiFjCz8BCARiy-xvR61tkozjQ0tKyFBQ0'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-	bot.sendMessage(msg.chat.id, `Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"`)
+	bot.sendMessage(msg.chat.id, `Hello, bot says: "Hi, ${msg.from.first_name}"`)
 })
